0354fa44e82bb61bd0de7cab8f6756d580e93a4e
KUN LI <likun9452@gmail.com>
2022-06-04 21:59:46 -0400

    umi-app

---
+++ .umirc.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    {
+      path: '/',
+      component: '@/layouts/index',
+      routes: [
+        {
+          path: '/',
+          component: './home/index',
+          title: '首页'
+        },
+        {
+          path: '/user',
+          component: './user/index',
+          title: '我的'
+        },
+        {
+          path: '/order',
+          component: './order/index',
+          title: '订单'
+        }
+      ]
+    }
+  ],
+  fastRefresh: {},
+});
---
+++ src/layouts/index.js
@@ -0,0 +1,24 @@
+import styles from './index.css';
+import { ErrorBoundary, MenuBar } from '@/components';
+import { useLocation } from 'umi';
+// import { StoreProvider } from 'think-react-store';
+// import * as store from '../stores';
+
+function BasicLayout(props) {
+    const location = useLocation();
+    const paths = ['/', '/order', '/user'];
+
+    return (
+        <div>
+            <MenuBar
+                show={paths.includes(location.pathname)}
+                pathname={location.pathname}
+            />
+            <ErrorBoundary>
+                {props.children}
+            </ErrorBoundary>
+        </div>
+    );
+}
+
+export default BasicLayout;
