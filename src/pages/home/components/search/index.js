998ea2abaa254b853db94492c1417ef1934a46fb
KUN LI <likun9452@gmail.com>
2022-06-07 00:22:40 -0400

    -add：mock data for homepage

--- src/pages/home/components/search/index.js
+++ src/pages/home/components/search/index.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Picker,List, Calendar,Button } from 'antd-mobile'
+import { Picker, List, Calendar, Button } from 'antd-mobile'
 import dayjs from 'dayjs';
 
-export default function(props){
-  const [citys, setCitys] = useState([[{label:'杭州',value:'10001'},{label:'苏州',value:'10002'}]])
-  const [selectedCity, setSelectedCity] = useState(['10001'])
+export default function (props) {
+  // console.log(props)
+  // const [citys, setCitys] = useState([[{ label: '杭州', value: '10001' }, { label: '苏州', value: '10002' }]]);
+  const [selectedCity, setSelectedCity] = useState(['10001']);
   const [times, setTimes] = useState('可选时间');
-  const [dateShow, setDateShow] = useState(false)
-  console.log(citys)
+  const [dateShow, setDateShow] = useState(false);
+  
   useEffect(() => {
-
   }, [])
   const handleCityChange = (val) => {
     setSelectedCity(val)
@@ -18,32 +18,35 @@ export default function(props){
     setDateShow(!dateShow)
   }
   const handleDateConfirm = (startTime, endTime) => {
-    setTimes(dayjs(startTime).format('YYYY-MM-DD')+ '-'+dayjs(endTime).format('YYYY-MM-DD')) 
+    setTimes(dayjs(startTime).format('YYYY-MM-DD') + '-' + dayjs(endTime).format('YYYY-MM-DD'))
     setDateShow(!dateShow)
   }
   return (
     <div className='search'>
-       {/* 可选城市 */}
-       <div className='search-addr'>
-         <Picker 
-         title={'城市'} 
-         data={citys} 
-         value={selectedCity} 
-         cascade={false}
-         cols={1}
-         onChange={handleCityChange}
-         >
-          <List.Item>可选城市</List.Item>
-         </Picker>
-       </div>
-       {/* 可选时间 */}
-       <div className='search-time' onClick={handleDate}>
+      {/* 可选城市 */}
+      <div className='search-addr'>
+        {
+          !props.citysLoading &&
+          <Picker
+            title={'城市'}
+            data={props.citys}
+            value={selectedCity}
+            cascade={false}
+            cols={1}
+            onChange={handleCityChange}>
+            <List.Item>可选城市</List.Item>
+          </Picker>
+        }
+
+      </div>
+      {/* 可选时间 */}
+      <div className='search-time' onClick={handleDate}>
         <p className='search-time_left'>出租时间</p>
         <p className='search-time_right'>{times}</p>
-       </div>
-       {/* 点击按钮 */}
-       <Button type='warning' size='large'>搜索民宿</Button>
-      <Calendar 
+      </div>
+      {/* 点击按钮 */}
+      <Button type='warning' size='large'>搜索民宿</Button>
+      <Calendar
         visible={dateShow}
         onCancel={handleDate}
         onConfirm={handleDateConfirm}
