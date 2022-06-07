5b322ffc3e94bb11d6d9281b1b7c9e118f81acd9
KUN LI <likun9452@gmail.com>
2022-06-06 22:58:54 -0400

    -add：homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import Header from './components/header'
+import Hot from './components/hot'
+import Search from './components/search'
+import './index.less';
 
 export default function(props){
   const [state, setState] = useState()
@@ -8,8 +12,13 @@ export default function(props){
   }, [])
 
   return (
-    <div>
-        home
+    <div className='home'>
+        {/* header 登录 */}
+        <Header />
+        {/* header 搜索 */}
+        <Search />
+        {/* header 热门民俗 */}
+        <Hot />
     </div>
   )
 }
