06a21ea68f4c45812baf2e4bc9bd70e753cca4f3
KUN LI <likun9452@gmail.com>
2022-06-20 00:30:52 -0400

    add: mock pay

--- src/pages/order/components/Item/index.js
+++ src/pages/order/components/Item/index.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'antd-mobile';
-import { timer } from '@/utils';
+import { Button, Toast } from 'antd-mobile';
+import { Http, timer } from '@/utils';
 
 export default function (props) {
   const [state, setState] = useState();
-
+  const handlePay = async () => {
+    const result = await Http({
+      url: '/orders/pay',
+      body: {
+        id: props.id,
+      },
+    });
+    if (result) {
+      Toast.success('支付成功');
+      window.location.reload();
+    }
+  };
   useEffect(() => {}, []);
   const renderPay = () => {
     switch (props.type) {
       case 0:
         return (
-          <Button type="warning" size="small">
+          <Button type="warning" size="small" onClick={handlePay}>
             去支付
           </Button>
         );
